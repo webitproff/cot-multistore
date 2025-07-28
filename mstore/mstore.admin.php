3a2573b6355ede428486af99fef40b55c2b59fe9
sodium carbonate <webitproff@gmail.com>
2025-07-28 19:07:41 +0300

    Add files via upload

---
+++ mstore/mstore.admin.php
@@ -0,0 +1,437 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Hooks=admin
+[END_COT_EXT]
+==================== */
+
+/**
+ * Mstore manager & Queue of store items
+ *
+ * @package Mstore
+ * @copyright (c) webitproff
+ * @license BSD
+ */
+
+use cot\modules\mstore\inc\MstoreDictionary;
+use cot\modules\mstore\inc\MstoreControlService;
+
+(defined('COT_CODE') && defined('COT_ADMIN')) or die('Wrong URL.');
+
+list(Cot::$usr['auth_read'], Cot::$usr['auth_write'], Cot::$usr['isadmin']) = cot_auth('mstore', 'any');
+cot_block(Cot::$usr['isadmin']);
+
+$t = new XTemplate(cot_tplfile('mstore.admin', 'module', true));
+
+require_once cot_incfile('mstore', 'module');
+
+$adminPath[] = [cot_url('admin', 'm=extensions'), Cot::$L['Extensions']];
+$adminPath[] = [cot_url('admin', 'm=extensions&a=details&mod='.$m), $cot_modules[$m]['title']];
+$adminPath[] = [cot_url('admin', 'm='.$m), Cot::$L['Administration']];
+$adminHelp = Cot::$L['adm_help_mstore'];
+$adminTitle = Cot::$L['Mstore'];
+
+$id = cot_import('id', 'G', 'INT');
+
+list($pg, $d, $durl) = cot_import_pagenav('d', Cot::$cfg['mstore']['mstoremaxlistsperpage']);
+
+$sorttype = cot_import('sorttype', 'R', 'ALP');
+$sorttype = empty($sorttype) ? 'id' : $sorttype;
+if ($sorttype != 'id' && !Cot::$db->fieldExists(Cot::$db->mstore, "msitem_$sorttype")) {
+	$sorttype = 'id';
+}
+$sqlsorttype = 'msitem_'.$sorttype;
+
+$sort_type = cot_mstore_config_order(true);
+
+$sortway = cot_import('sortway', 'R', 'ALP');
+$sortway = empty($sortway) ? 'desc' : $sortway;
+$sort_way = [
+	'asc' => Cot::$L['Ascending'],
+	'desc' => Cot::$L['Descending'],
+];
+$sqlsortway = $sortway;
+
+$filter = cot_import('filter', 'R', 'ALP');
+$filter = empty($filter) ? 'valqueue' : $filter;
+$filter_type = [
+	'all' => Cot::$L['All'],
+	'valqueue' => Cot::$L['adm_valqueue'],
+	'validated' => Cot::$L['adm_validated'],
+	'expired' => Cot::$L['adm_expired'],
+	'drafts' => Cot::$L['mstore_drafts'],
+];
+
+$urlParams = ['m' => 'mstore'];
+if ($sorttype != 'id') {
+    $urlParams['sorttype'] = $sorttype;
+}
+if ($sortway != 'desc') {
+    $urlParams['sortway'] = $sortway;
+}
+if ($filter != 'valqueue') {
+    $urlParams['filter'] = $filter;
+}
+
+/**
+ * Common UrlParams without pagination
+ * @deprecated
+ */
+$common_params = http_build_query($urlParams, '', '&');
+
+if ($pg > 1) {
+    $urlParams['d'] = $durl;
+}
+
+if ($filter == 'all') {
+	$sqlwhere = "1 ";
+} elseif ($filter == 'valqueue') {
+	$sqlwhere = 'msitem_state = ' . MstoreDictionary::STATE_PENDING ;
+} elseif ($filter == 'validated') {
+	$sqlwhere = 'msitem_state = ' . MstoreDictionary::STATE_PUBLISHED ;
+} elseif ($filter == 'drafts') {
+	$sqlwhere = 'msitem_state = ' . MstoreDictionary::STATE_DRAFT;
+} elseif ($filter == 'expired') {
+	$sqlwhere = "msitem_begin > {$sys['now']} OR (msitem_expire <> 0 AND msitem_expire < {$sys['now']})";
+}
+
+$catsub = cot_structure_children('mstore', '');
+if (count($catsub) < count(Cot::$structure['mstore'])) {
+	$sqlwhere .= " AND msitem_cat IN ('" . implode("','", $catsub) . "')";
+}
+
+$backUrl = cot_import('back', 'G', 'HTM');
+$backUrl = !empty($backUrl) ?
+    base64_decode($backUrl) : cot_url('admin', $urlParams, '', true);
+
+/* === Hook  === */
+foreach (cot_getextplugins('mstore.admin.first') as $pl) {
+	include $pl;
+}
+/* ===== */
+
+if ($a == 'validate') {
+	cot_check_xg();
+
+	/* === Hook  === */
+	foreach (cot_getextplugins('mstore.admin.validate') as $pl) {
+		include $pl;
+	}
+	/* ===== */
+
+    $row = Cot::$db->query(
+        'SELECT msitem_id, msitem_alias, msitem_cat, msitem_begin, msitem_state FROM ' . Cot::$db->mstore
+        . ' WHERE msitem_id = ?',
+        $id
+    )->fetch();
+	if ($row) {
+        if ($row['msitem_state'] == MstoreDictionary::STATE_PUBLISHED) {
+            cot_message('#' . $id . ' - ' . Cot::$L['adm_already_updated']);
+            cot_redirect($backUrl);
+        }
+
+		$usr['isadmin_local'] = cot_auth('mstore', $row['msitem_cat'], 'A');
+		cot_block($usr['isadmin_local']);
+        $data = ['msitem_state' => MstoreDictionary::STATE_PUBLISHED];
+		if ($row['msitem_begin'] < Cot::$sys['now']) {
+            $data['msitem_begin'] = Cot::$sys['now'];
+		}
+		$sql_mstore = Cot::$db->update(Cot::$db->mstore, $data, "msitem_id = $id");
+
+		/* === Hook  === */
+		foreach (cot_getextplugins('mstore.admin.validate.done') as $pl) {
+			include $pl;
+		}
+		/* ===== */
+
+		cot_log(
+            Cot::$L['Mstore'] . ' #' . $id . ' - ' . Cot::$L['adm_queue_validated'],
+            'mstore',
+            'validate',
+            'done'
+        );
+
+		if (Cot::$cache) {
+            Cot::$cache->db->remove('structure', 'system');
+			if (Cot::$cfg['cache_mstore']) {
+                Cot::$cache->static->clearByUri(cot_mstore_url($row));
+                Cot::$cache->static->clearByUri(cot_url('mstore', ['c' => $row['msitem_cat']]));
+			}
+			if (Cot::$cfg['cache_index']) {
+                Cot::$cache->static->clear('index');
+			}
+		}
+		cot_message('#' . $id . ' - ' . Cot::$L['adm_queue_validated']);
+
+	} else {
+        cot_error('#' . $id . ' - ' . Cot::$L['nf']);
+	}
+
+    cot_redirect($backUrl);
+} elseif ($a == 'unvalidate') {
+	cot_check_xg();
+
+	/* === Hook  === */
+	foreach (cot_getextplugins('mstore.admin.unvalidate') as $pl) {
+		include $pl;
+	}
+	/* ===== */
+
+    $row = Cot::$db->query(
+        'SELECT msitem_id, msitem_alias, msitem_cat, msitem_state FROM ' . Cot::$db->mstore . ' WHERE msitem_id = ?',
+        $id
+    )->fetch();
+    if ($row) {
+        if ($row['msitem_state'] == MstoreDictionary::STATE_PENDING) {
+            cot_message('#' . $id . ' - ' . Cot::$L['adm_already_updated']);
+            cot_redirect($backUrl);
+        }
+
+		Cot::$usr['isadmin_local'] = cot_auth('mstore', $row['msitem_cat'], 'A');
+		cot_block($usr['isadmin_local']);
+
+		$sql_mstore = Cot::$db->update(
+            Cot::$db->mstore,
+            ['msitem_state' => MstoreDictionary::STATE_PENDING],
+            'msitem_id = ?',
+            $id
+        );
+
+		cot_log(Cot::$L['Mstore'] . ' #' . $id . ' - ' . Cot::$L['adm_queue_unvalidated'], 'mstore', 'unvalidated', 'done');
+
+		if (Cot::$cache) {
+            Cot::$cache->db->remove('structure', 'system');
+			if (Cot::$cfg['cache_mstore']) {
+                Cot::$cache->static->clearByUri(cot_mstore_url($row));
+                Cot::$cache->static->clearByUri(cot_url('mstore', ['c' => $row['msitem_cat']]));
+			}
+			if (Cot::$cfg['cache_index']) {
+                Cot::$cache->static->clear('index');
+			}
+		}
+
+		cot_message('#' . $id . ' - ' . Cot::$L['adm_queue_unvalidated']);
+
+    } else {
+        cot_error('#' . $id . ' - ' . Cot::$L['nf']);
+	}
+
+    cot_redirect($backUrl);
+} elseif ($a == 'delete') {
+	cot_check_xg();
+
+	/* === Hook  === */
+	foreach (cot_getextplugins('mstore.admin.delete') as $pl) {
+		include $pl;
+	}
+	/* ===== */
+
+    $resultOrMessage = MstoreControlService::getInstance()->delete($id);
+    if ($resultOrMessage !== false) {
+        /* === Hook === */
+		foreach (cot_getextplugins('mstore.admin.delete.done') as $pl) {
+			include $pl;
+		}
+		/* ===== */
+
+        cot_message('#' . $id . ' - ' . $resultOrMessage);
+    } else {
+        cot_error('#' . $id . ' - ' . Cot::$L['adm_failed']);
+    }
+
+    cot_redirect(cot_url('admin', $urlParams, '', true));
+} elseif ($a == 'update_checked') {
+	$paction = cot_import('paction', 'P', 'TXT');
+	$s = cot_import('s', 'P', 'ARR');
+
+	if ($paction == 'validate' && is_array($s)) {
+		cot_check_xp();
+
+		$perelik = '';
+		$notfoundet = '';
+		foreach ($s as $i => $k) {
+			if ($s[$i] == '1' || $s[$i] == 'on') {
+				/* === Hook  === */
+				foreach (cot_getextplugins('mstore.admin.checked_validate') as $pl) {
+					include $pl;
+				}
+				/* ===== */
+
+				$sql_mstore = Cot::$db->query('SELECT * FROM ' . Cot::$db->mstore . ' WHERE msitem_id = ?', $i);
+				if ($row = $sql_mstore->fetch()) {
+					$id = $row['msitem_id'];
+					$usr['isadmin_local'] = cot_auth('mstore', $row['msitem_cat'], 'A');
+					cot_block($usr['isadmin_local']);
+
+					$sql_mstore = Cot::$db->update(
+                        Cot::$db->mstore,
+                        ['msitem_state' => MstoreDictionary::STATE_PUBLISHED],
+                        'msitem_id= ?',
+                        $id
+                    );
+
+					cot_log(
+                        Cot::$L['Mstore'] . ' #' . $id . ' - ' . Cot::$L['adm_queue_validated'],
+                        'mstore',
+                        'validate',
+                        'done'
+                    );
+
+					if (Cot::$cache && Cot::$cfg['cache_mstore']) {
+                        Cot::$cache->static->clearByUri(cot_mstore_url($row));
+                        Cot::$cache->static->clearByUri(cot_url('mstore', ['c' => $row['msitem_cat']]));
+					}
+
+					$perelik .= '#' . $id . ', ';
+				} else {
+					$notfoundet .= '#' . $id . ' - ' . Cot::$L['Error'] . '<br  />';
+				}
+			}
+		}
+
+        if (Cot::$cache) {
+            Cot::$cache->db->remove('structure', 'system');
+            if (Cot::$cfg['cache_index']) {
+                Cot::$cache->static->clear('index');
+            }
+        }
+
+        if (!empty($notfoundet)) {
+            cot_error($notfoundet);
+        }
+
+		if (!empty($perelik)) {
+			cot_message($perelik . ' - ' . Cot::$L['adm_queue_validated']);
+		}
+
+        cot_redirect(cot_url('admin', $urlParams, '', true));
+	} elseif ($paction == 'delete' && is_array($s)) {
+		cot_check_xp();
+
+		$perelik = '';
+		$notfoundet = '';
+        $mstoreService = MstoreControlService::getInstance();
+		foreach ($s as $id => $k) {
+			if ($s[$id] == '1' || $s[$id] == 'on') {
+
+				/* === Hook  === */
+				foreach (cot_getextplugins('mstore.admin.checked_delete') as $pl) {
+					include $pl;
+				}
+				/* ===== */
+
+                $resultOrMessage = $mstoreService->delete((int) $id);
+                if ($resultOrMessage !== false) {
+                    /* === Hook === */
+                    foreach (cot_getextplugins('mstore.admin.delete.done') as $pl) {
+                        include $pl;
+                    }
+                    /* ===== */
+                    if ($perelik !== '') {
+                        $perelik .= ', ';
+                    }
+                    $perelik .= '#' . $id;
+                } else {
+                    $notfoundet .= '#'. $id . ' - ' . Cot::$L['Error'] . '<br  />';
+                }
+			}
+		}
+
+        if (!empty($notfoundet)) {
+            cot_error($notfoundet);
+        }
+
+        if (!empty($perelik)) {
+            cot_message($perelik . ' - ' . Cot::$L['mstore_deleted']);
+        }
+
+        cot_redirect(cot_url('admin', $urlParams, '', true));
+	}
+}
+
+$totalitems = Cot::$db->query('SELECT COUNT(*) FROM ' . Cot::$db->mstore . ' WHERE ' . $sqlwhere)->fetchColumn();
+$pagenav = cot_pagenav(
+	'admin',
+	$common_params,
+	$d,
+	$totalitems,
+	Cot::$cfg['mstore']['mstoremaxlistsperpage'],
+	'd',
+	'',
+	Cot::$cfg['jquery'] && Cot::$cfg['turnajax']
+);
+
+$sql_mstore = Cot::$db->query("SELECT p.*, u.user_name
+	FROM $db_mstore as p
+	LEFT JOIN $db_users AS u ON u.user_id = p.msitem_ownerid
+	WHERE $sqlwhere
+		ORDER BY $sqlsorttype $sqlsortway
+		LIMIT $d, ".Cot::$cfg['mstore']['mstoremaxlistsperpage']);
+
+$ii = 0;
+/* === Hook - Part1 : Set === */
+$extp = cot_getextplugins('mstore.admin.loop');
+/* ===== */
+foreach ($sql_mstore->fetchAll() as $row) {
+    $sub_count = 0;
+    if (isset(Cot::$structure['mstore'][$row["msitem_cat"]])) {
+        $sql_mstore_subcount = Cot::$db->query("SELECT SUM(structure_count) FROM $db_structure WHERE structure_path LIKE '" .
+                Cot::$db->prep(Cot::$structure['mstore'][$row["msitem_cat"]]['rpath']) . "%' ");
+        $sub_count = $sql_mstore_subcount->fetchColumn();
+    }
+	$row['msitem_file'] = intval($row['msitem_file']);
+	$t->assign(cot_generate_mstoretags($row, 'ADMIN_MSTORE_', 200));
+	$t->assign([
+		'ADMIN_MSTORE_ID_URL' => cot_url('mstore', 'c=' . $row['msitem_cat'] . '&id=' . $row['msitem_id']),
+		'ADMIN_MSTORE_OWNER' => cot_build_user($row['msitem_ownerid'], $row['user_name']),
+		'ADMIN_MSTORE_FILE_BOOL' => $row['msitem_file'],
+		'ADMIN_MSTORE_URL_FOR_VALIDATED' => cot_confirm_url(cot_url('admin', $common_params . '&a=validate&id=' . $row['msitem_id'] . '&d=' . $durl . '&' . cot_xg()), 'mstore', 'mstore_confirm_validate'),
+		'ADMIN_MSTORE_URL_FOR_UNVALIDATE' => cot_confirm_url(cot_url('admin', $common_params . '&a=unvalidate&id=' . $row['msitem_id'] . '&d=' . $durl . '&' . cot_xg()), 'mstore', 'mstore_confirm_unvalidate'),
+		'ADMIN_MSTORE_URL_FOR_DELETED' => cot_confirm_url(cot_url('admin', $common_params . '&a=delete&id=' . $row['msitem_id'] . '&d=' . $durl . '&' . cot_xg()), 'mstore', 'mstore_confirm_delete'),
+		'ADMIN_MSTORE_URL_FOR_EDIT' => cot_url('mstore', 'm=edit&id=' . $row['msitem_id']),
+		'ADMIN_MSTORE_ODDEVEN' => cot_build_oddeven($ii),
+		'ADMIN_MSTORE_CAT_COUNT' => $sub_count,
+	]);
+	$t->assign(cot_generate_usertags($row['msitem_ownerid'], 'ADMIN_MSTORE_OWNER_'), htmlspecialchars($row['user_name']));
+
+	/* === Hook - Part2 : Include === */
+	foreach ($extp as $pl) {
+		include $pl;
+	}
+	/* ===== */
+
+	$t->parse('MAIN.MSTORE_ROW');
+	$ii++;
+}
+
+$totaldbitems = Cot::$db->countRows($db_mstore);
+$sql_mstore_queued = Cot::$db->query(
+    'SELECT COUNT(*) FROM ' . Cot::$db->mstore . ' WHERE msitem_state = ' . MstoreDictionary::STATE_PENDING
+);
+$sys['mstorequeued'] = $sql_mstore_queued->fetchColumn();
+
+$t->assign([
+	'ADMIN_MSTORE_URL_CONFIG' => cot_url('admin', 'm=config&n=edit&o=module&p=mstore'),
+	'ADMIN_MSTORE_URL_ADD' => cot_url('mstore', 'm=add'),
+	'ADMIN_MSTORE_URL_EXTRAFIELDS' => cot_url('admin', 'm=extrafields&n=' . $db_mstore),
+	'ADMIN_MSTORE_URL_STRUCTURE' => cot_url('admin', 'm=structure&n=mstore'),
+	'ADMIN_MSTORE_FORM_URL' => cot_url('admin', $common_params.'&a=update_checked&d=' . $durl),
+	'ADMIN_MSTORE_ORDER' => cot_selectbox($sorttype, 'sorttype', array_keys($sort_type), array_values($sort_type), false),
+	'ADMIN_MSTORE_WAY' => cot_selectbox($sortway, 'sortway', array_keys($sort_way), array_values($sort_way), false),
+	'ADMIN_MSTORE_FILTER' => cot_selectbox($filter, 'filter', array_keys($filter_type), array_values($filter_type), false),
+	'ADMIN_MSTORE_TOTALDBITEMS' => $totaldbitems,
+    'ADMIN_MSTORE_ON_PAGE' => $ii,
+]);
+
+$t->assign(cot_generatePaginationTags($pagenav));
+
+cot_display_messages($t);
+
+/* === Hook  === */
+foreach (cot_getextplugins('mstore.admin.tags') as $pl) {
+	include $pl;
+}
+/* ===== */
+
+$t->parse('MAIN');
+$adminMain = $t->text('MAIN');
---
+++ mstore/setup/mstore.install.sql
@@ -0,0 +1,79 @@
+/**
+ * Mstore module DB installation
+ */
+
+-- Default categories permissions
+INSERT INTO `cot_auth` (`auth_groupid`, `auth_code`, `auth_option`, `auth_rights`, `auth_rights_lock`, `auth_setbyuserid`) VALUES
+(1, 'mstore', 'e-scooter', 5, 250, 1),
+(2, 'mstore', 'e-scooter', 1, 254, 1),
+(3, 'mstore', 'e-scooter', 0, 255, 1),
+(4, 'mstore', 'e-scooter', 7, 0, 1),
+(5, 'mstore', 'e-scooter', 255, 255, 1),
+(6, 'mstore', 'e-scooter', 135, 0, 1),
+(1, 'mstore', 'e-bike', 5, 250, 1),
+(2, 'mstore', 'e-bike', 1, 254, 1),
+(3, 'mstore', 'e-bike', 0, 255, 1),
+(4, 'mstore', 'e-bike', 7, 0, 1),
+(5, 'mstore', 'e-bike', 255, 255, 1),
+(6, 'mstore', 'e-bike', 135, 0, 1),
+(1, 'mstore', 'system', 5, 250, 1),
+(2, 'mstore', 'system', 1, 254, 1),
+(3, 'mstore', 'system', 0, 255, 1),
+(4, 'mstore', 'system', 7, 0, 1),
+(5, 'mstore', 'system', 255, 255, 1),
+(6, 'mstore', 'system', 135, 0, 1),
+(1, 'mstore', 'tricycle', 5, 250, 1),
+(2, 'mstore', 'tricycle', 1, 254, 1),
+(3, 'mstore', 'tricycle', 0, 255, 1),
+(4, 'mstore', 'tricycle', 7, 0, 1),
+(5, 'mstore', 'tricycle', 255, 255, 1),
+(6, 'mstore', 'tricycle', 135, 0, 1);
+
+-- Store items table
+CREATE TABLE IF NOT EXISTS `cot_mstore` (
+  `msitem_id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `msitem_alias` varchar(255) NOT NULL DEFAULT '',
+  `msitem_state` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_cat` varchar(255) NOT NULL,
+  `msitem_title` varchar(255) NOT NULL,
+  `msitem_desc` varchar(255) DEFAULT '',
+  `msitem_keywords` varchar(255) DEFAULT '',
+  `msitem_metatitle` varchar(255) DEFAULT '',
+  `msitem_metadesc` varchar(255) DEFAULT '',
+  `msitem_text` MEDIUMTEXT DEFAULT NULL,
+  `msitem_price_opt` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `msitem_price_drop` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `msitem_price_rr` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `msitem_parser` varchar(64) DEFAULT '',
+  `msitem_author` varchar(100) DEFAULT '',
+  `msitem_ownerid` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_date` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_begin` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_expire` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_updated` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_main_image_url` varchar(255) DEFAULT '',
+  `msitem_file` tinyint UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_url` varchar(255) DEFAULT '',
+  `msitem_size` int UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_count` mediumint UNSIGNED NOT NULL DEFAULT '0',
+  `msitem_filecount` mediumint UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`msitem_id`),
+  KEY `msitem_cat` (`msitem_cat`),
+  KEY `msitem_alias` (`msitem_alias`),
+  KEY `msitem_date` (`msitem_date`),
+  KEY `msitem_ownerid` (`msitem_ownerid`),
+  KEY `msitem_begin` (`msitem_begin`),
+  KEY `msitem_expire` (`msitem_expire`),
+  KEY `msitem_title` (`msitem_title`)
+);
+
+-- Default store categories
+INSERT INTO `cot_structure` (`structure_area`, `structure_code`, `structure_path`, `structure_tpl`, `structure_title`, `structure_desc`, `structure_icon`, `structure_locked`, `structure_count`) VALUES
+('mstore', 'e-scooter', '001', '', 'E-scooter', 'Electric scooters for urban mobility', '', 0, 0),
+('mstore', 'system', '999', '', 'System', 'System category for internal use', '', 0, 0),
+('mstore', 'e-bike', '002', '', 'E-bike', 'Electric bicycles for eco-friendly transport', '', 0, 0),
+('mstore', 'tricycle', '003', '', 'Tricycle', 'Three-wheeled electric vehicles for stability', '', 0, 1);
+
+-- Sample store item
+INSERT INTO `cot_mstore` (`msitem_state`, `msitem_cat`, `msitem_title`, `msitem_desc`, `msitem_text`, `msitem_price_opt`, `msitem_price_drop`, `msitem_price_rr`, `msitem_author`, `msitem_ownerid`, `msitem_date`, `msitem_begin`, `msitem_expire`, `msitem_file`, `msitem_url`, `msitem_size`, `msitem_count`, `msitem_filecount`, `msitem_alias`) VALUES
+(0, 'tricycle', 'Crosser TR7 Tricycle', 'Stable and modern electric tricycle', 'The Crosser TR7 electric tricycle is an excellent choice for those seeking stability, safety, and modern design for urban or suburban routes. This electric trike is designed with a focus on comfort and confidence while driving. Thanks to modern technologies and reliable components, the TR7 combines the best qualities of a scooter, moped, and convenient everyday transport.', '0.00', '0.00', '0.00', '', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), 0, 0, '', 0, 0, 0, '');
